79cf53bd92e7be5622d63cdd3a09ee74e758706f
dionygithub <dionyuci@gmail.com>
2016-09-13 18:20:11 -0400

    Add files via upload

--- wp-content/themes/html5blank/header.php
+++ wp-content/themes/html5blank/header.php
@@ -39,7 +39,8 @@
 						</div>
 					</nav>
 					<div class="flags">
-	                	<!-- NEGRIN PONES AQUI LA REGION PARA EL WIDGETS DEL IDIOMA -->
+	                	<?php if (!function_exists('dynamic_sidebar') || !dynamic_sidebar('Widget Area 2')) : ?>
+						<?php endif; ?>
 	                </div>
 				</div>
 			</div>
